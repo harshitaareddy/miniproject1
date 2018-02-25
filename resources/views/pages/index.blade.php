a2db728f7985bbe7fe9352fd978306246032c4d2
harshita <hk389@njit.edu>
2018-02-25 03:54:21 -0500

    sigin and signup

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-3">Welcome to the world of fitness</h1>
+            <img src="https://i.ytimg.com/vi/ylemoDMqBzU/maxresdefault.jpg" alt="Girl in  gym" style="width:1000px;height:400px;">
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Zumba</h2>
+                <p>ZUMBA® is a fusion of Latin and International music / dance themes that create a dynamic, exciting, and based on the principle that a workout should be "FUN AND EASY TO DO." </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Aerobics</h2>
+                <p>Creative Cardio. (Hi/Low impact dance aerobics) - is an energetic, upbeat aerobics class that is sure to get your body moving. A great workout for all levels of fitness enthusiasts, this class combines cardiovascular training and toning exercises, for a superior total body workout. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Yoga</h2>
+                <p>YogaFit is designed to improve the health, performance, and mental acuity of athletes or individuals interested in improving their level of fitness. Based on the ancient fitness science of hatha yoga, it blends balance, strength, flexibility and power in a fitness format.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+        </div>
+
+        <hr>
+
+    </div> <!-- /container -->
+
+@endsection
